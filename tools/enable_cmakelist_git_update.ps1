86b8b0247f8c52ac7c20e57cc8be378687739c95
Dave Glover <dglover.au@microsoft.com>
2021-07-13 16:48:17 +1000

    updated to fix stop cmake and certs updates added to repo

---
+++ tools/enable_cmakelist_git_update.ps1
@@ -0,0 +1,9 @@
+﻿echo "`n`nSetting --assume-unchanged on all cmakelists.txt files"
+echo "cmakelists.txt files will NOT be pushed/synced by git`n`n"
+
+$fileList = Get-ChildItem -Include cmakelists.txt -Recurse  -Name -Depth 2
+foreach ($file in $fileList) 
+{
+    echo $file
+    git update-index --no-assume-unchanged $file
+}
