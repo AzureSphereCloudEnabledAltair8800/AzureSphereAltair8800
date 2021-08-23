7d3b05d25244745af3c17ef8f1f95d56d5600c37
Dave Glover <dglover@microsoft.com>
2021-08-24 09:15:43 +1000

    removed enviromon rt app - now done in hl emulator

--- Altair_Deploy_OTA/config.ps1
+++ Altair_Deploy_OTA/config.ps1
@@ -10,5 +10,3 @@ $global:device_group = "<REPLACE_WITH_YOUR_DEVICE_GROUP_ID>"
 $global:altair_emulator_image_path_filename = "..\AltairHL_emulator\out\ARM-Release\AltairHL_emulator.imagepackage"
 # Altair RT Disk Cache Server
 $global:altair_disk_cache_image_path_filename = "..\AltairRT_disk_cache_server\out\ARM-Release\AltairRT_disk_cache_server.imagepackage"
-# Altair RT Environment Monitor
-$global:altair_enviromon_image_path_filename = "..\AltairRT_enviromon\out\ARM-Release\AltairRT_enviromon.imagepackage"
