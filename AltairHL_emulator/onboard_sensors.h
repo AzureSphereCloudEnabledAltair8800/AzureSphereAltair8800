0b0862c315073dfbe562ecaabc0e822546ad580d
Dave Glover <dglover@microsoft.com>
2021-08-23 12:48:32 +1000

    initial support for onboard sensors for emulator

---
+++ AltairHL_emulator/onboard_sensors.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <stdbool.h>
+#include <stdlib.h>
+#include <time.h>
+
+#ifdef OEM_AVNET
+#include "./Drivers/AVNET/HL/imu_temp_pressure.h"
+#endif
+
+typedef struct {
+    int temperature;
+    int humidity;
+    int pressure;
+} ONBOARD_TELEMETRY;
+
+bool onboard_sensors_read(ONBOARD_TELEMETRY *telemetry);
+bool onboard_sensors_init(void);
+bool onboard_sensors_close(void);
