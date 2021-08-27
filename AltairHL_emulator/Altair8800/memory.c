4e1b22ef8ea0a5c5724ccaa63c3a100d47e1e98a
Dave Glover <dglover@microsoft.com>
2021-08-27 11:30:42 +1000

    Update memory.c
    Improved memory access. Memory bounds checks removed as 16-bit address requests will always be valid given 2^16 bit address space allocated for the Intel 8080 emulator

--- AltairHL_emulator/Altair8800/memory.c
+++ AltairHL_emulator/Altair8800/memory.c
@@ -4,42 +4,25 @@ extern uint8_t memory[64 * 1024];
 
 uint8_t read8(uint16_t address)
 {
-    uint8_t data;
-    if (address < 64 * 1024) {
-        data = memory[address];
-    } else {
-        data = 0;
-    }
-
-    return data;
+    return memory[address];
 }
 
 void write8(uint16_t address, uint8_t val)
 {
-    if (address < 64 * 1024) {
-        memory[address] = val;
-    }
+    memory[address] = val;
 }
 
 // #endif
 
 uint16_t read16(uint16_t address)
 {
-    if (address < (64 * 1024) - 1) {
-        uint16_t result = 0;
-        result = read8(address);
-        result |= read8(address + 1) << 8;
-        return result;
-    } else {
-        return 0;
-    }    
+    uint16_t result = read8(address);
+    result |= read8(address + 1) << 8;
+    return result;
 }
 
 void write16(uint16_t address, uint16_t val)
 {
-    if (address < (64 * 1024) - 1) {
-        write8(address, val & 0xff);
-        write8(address + 1, (val >> 8) & 0xff);
-    }
+    write8(address, val & 0xff);
+    write8(address + 1, (val >> 8) & 0xff);
 }
-
